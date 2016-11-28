d72c52ce9278e68af7f9be2a068786e08e049e9b
Mike D <tontarelli@gmail.com>
2016-11-28 19:39:57 +0200

    comm5

--- mishanja/Lesson5/app.js
+++ mishanja/Lesson5/app.js
@@ -1,48 +1,37 @@
 const http = require('http');
 const url = require('url');
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 8080;
-var fs = require('fs');
-var obj;
 
 
-fs.exists("data.json", function(fileok){
-    if(fileok)fs.readFile("data.json", function(error, data) {
-        console.log("Contents: " + data);
-        obj = JSON.parse(data);
-        console.log(obj.name);
-    });
-    else console.log("file not found");
-});
-console.log("Carry on executing");
-
-fs.readFile('data.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    obj = JSON.parse(data);
-    console.log(obj.name);
-});
-
-const server = http.createServer(function(req, res) {
+const server = http.createServer((req, res) => {
     console.log( req.method, req.url);
 
     var parcedUrl = url.parse(req.url, true);
     console.log(parcedUrl);
-    if (parcedUrl.pathname === '/data/'){
-        res.statusCode = 200;
+    if (parcedUrl.pathname === '/data'){
 
-        console.log("Well...");
-        res.end(
-        '<title>WebApp</title>' +
-            '<body><h1>' + obj.message + '\n' + obj.count +'</h1></body>'
-        );
+        fs.readFile('./data.json', 'utf8', function (err, data) {
+            if (err) throw err;
+            res.statusCode = 200;
+
+            res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+        });
     } else {
+        fs.readFile('index.html', function (err, html) {
+            if (err) {
+                throw err;
+            }
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World\n');
+        res.end(html);
+        });
+
     }
 });
 
-server.listen(port, hostname); /*, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-}); */
+});
